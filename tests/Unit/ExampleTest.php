3ef8c70a7fd0f082de763c6b9f198e7e149f30d9
Yacine-df <ycn.diaf@gmail.com>
2024-08-17 19:18:13 +0200

    WIP

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yacinediaf\LaravelLikeable\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
