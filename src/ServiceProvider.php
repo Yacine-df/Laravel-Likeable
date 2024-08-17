555c18cb9744b8c6466c19bfd2f4c17ef892fef5
Yacine-df <ycn.diaf@gmail.com>
2024-08-17 20:52:58 +0200

    Extend laravel Service proivder

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Yacinediaf\LaravelLikeable;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvide extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
